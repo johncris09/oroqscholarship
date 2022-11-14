1a75029de27f460d26859988a6d2747898f83c08
John Cris <johncrismanabo9@gmail.com>
2022-11-14 16:40:36 +0800

    git clone

--- app/Views/admin/school.php
+++ app/Views/admin/school.php
@@ -65,51 +65,51 @@
 
 <?= $this->section('schooljs') ?>
 
-<script>
-    $(document).ready(function() {
-        $('#school-table').DataTable({
-            "scrollY": 450,
-            "scrollX": true, 
-            deferRender: true, 
-            ajax: {
-                url: 'school/get_all',  
-            },
-            columns: [ 
-                { data: 'ID' },  
-                { data: 'SchoolName' },  
-                { data: 'Manager' },  
-            ], 
-            columnDefs: [
-                {
-                    targets  : 0,
-                    title    : 'Actions',
-                    orderable: false,
-                    render   : function(data, type, row, meta) { 
-                        var action = '';
-                    
-                        action =  '\
-                                <div class="btn-group">\
-                                    <button type="button" class="btn dropdown-toggle text-primary" data-bs-toggle="dropdown" aria-expanded="false"> <i class="mdi mdi-cog"></i>  <i class="mdi mdi-chevron-down"></i> </button>\
-                                    <div class="dropdown-menu">\
-                                        <a class="dropdown-item text-warning" href="#">\
-                                            <i class="mdi mdi-grease-pencil"></i>\
-                                            <span class="nav-text">Edit Details</span>\
-                                        </a>\
-                                        <a class="dropdown-item text-danger" href="#">\
-                                            <i class="mdi mdi-grease-pencil"></i>\
-                                            <span class="nav-text">Delete</span>\
-                                        </a>\
+    <script>
+        $(document).ready(function() {
+            $('#school-table').DataTable({
+                "scrollY": 450,
+                "scrollX": true, 
+                deferRender: true, 
+                ajax: {
+                    url: 'school/get_all',  
+                },
+                columns: [ 
+                    { data: 'ID' },  
+                    { data: 'SchoolName' },  
+                    { data: 'Manager' },  
+                ], 
+                columnDefs: [
+                    {
+                        targets  : 0,
+                        title    : 'Actions',
+                        orderable: false,
+                        render   : function(data, type, row, meta) { 
+                            var action = '';
+                        
+                            action =  '\
+                                    <div class="btn-group">\
+                                        <button type="button" class="btn dropdown-toggle text-primary" data-bs-toggle="dropdown" aria-expanded="false"> <i class="mdi mdi-cog"></i>  <i class="mdi mdi-chevron-down"></i> </button>\
+                                        <div class="dropdown-menu">\
+                                            <a class="dropdown-item text-warning" href="#">\
+                                                <i class="mdi mdi-grease-pencil"></i>\
+                                                <span class="nav-text">Edit Details</span>\
+                                            </a>\
+                                            <a class="dropdown-item text-danger" href="#">\
+                                                <i class="mdi mdi-grease-pencil"></i>\
+                                                <span class="nav-text">Delete</span>\
+                                            </a>\
+                                        </div>\
                                     </div>\
-                                </div>\
-                            '; 
-                            return action;
-                        }, 
-                },  
-            ],
-        });  
+                                '; 
+                                return action;
+                            }, 
+                    },  
+                ],
+            });  
 
-    });
-</script>
+        });
+    </script>
 
 <?= $this->endSection() ?>
 
