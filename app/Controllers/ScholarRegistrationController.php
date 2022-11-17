50e6208a26886bf4df193ff8d5d7cbc8508d771a
John Cris <johncrismanabo9@gmail.com>
2022-11-17 09:35:43 +0800

    load custom config

--- app/Controllers/ScholarRegistrationController.php
+++ app/Controllers/ScholarRegistrationController.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\SchoolModel;
+use App\Models\StrandModel;
+use Config\Custom_config;
 
 class ScholarRegistrationController extends BaseController
 {
@@ -10,6 +13,15 @@ class ScholarRegistrationController extends BaseController
     public function index()
     {     
         $data["page_title"] = "Scholar Registration";
+        $config = new Custom_config;
+        $school = new SchoolModel();
+        $strand = new StrandModel();
+        $data['barangay'] = $config->barangay; 
+        $data['semester'] = $config->semester; 
+        $data['civil_status'] = $config->civilStatus; 
+        $data['grade_level'] = $config->gradeLevel;
+        $data['school'] = $school->asArray()->findAll();
+        $data['strand'] = $strand->asArray()->findAll();
         return view('admin/scholar_registration', $data); 
     } 
-}
+} 
