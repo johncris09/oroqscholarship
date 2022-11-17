c9d78fa3d5684921d717618ea6416e2b0e1a7dc8
John Cris <johncrismanabo9@gmail.com>
2022-11-17 12:25:33 +0800

    tvet registration form

--- app/Views/admin/scholar_registration.php
+++ app/Views/admin/scholar_registration.php
@@ -4,7 +4,7 @@
 
 <?= $this->section('pageStyle') ?>
     <style>
-        #frameShs:hover, #frameCollege:hover{
+        #frameShs:hover, #frameCollege:hover, #frameTvet:hover{
             cursor: pointer;
         }
     </style>
@@ -207,8 +207,7 @@
                             </div>    
                         </form>
                     </div>
-                    <div class="tab-pane show active " id="college-tab"> 
-                        
+                    <div class="tab-pane show active " id="college-tab">  
                         <form id="college-registration-form" class="validation-form">
                             <div class="row">
                                 <div class="col-9">
@@ -394,6 +393,186 @@
                     
                     <div class="tab-pane" id="tvet-tab">
                         
+                    <form id="college-registration-form" class="validation-form">
+                            <div class="row">
+                                <div class="col-9">
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <label for="lastname" class="form-label">Last Name <?= $required_field; ?></label>
+                                            <input type="text" class="form-control" id="lastname" required>
+                                        </div>
+                                        <div class="col-4">
+                                            <label for="firstname" class="form-label">First Name <?= $required_field; ?></label>
+                                            <input type="text" class="form-control" id="firstname" required>
+                                        </div>
+                                        <div class="col-2">
+                                            <label for="middlename" class="form-label">M.I.</label>
+                                            <input type="text" class="form-control" id="middlename">
+                                        </div>
+                                        <div class="col-2">
+                                            <label for="suffix" class="form-label">Suffix</label>
+                                            <input type="text" class="form-control" id="suffix">
+                                        </div> 
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-12">  
+                                            <label for="barangay" class="form-label">Address <?= $required_field; ?></label>
+                                            <select class="form-control" name="barangay" id="barangay" required>
+                                                <option value="">Select</option> 
+                                                <?php foreach($barangay as $row):?> 
+                                                    <option value=""><?= $row ?></option>  
+                                                <?php endforeach; ?>
+                                            </select>
+                                        </div>  
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <label for=""  class="form-label">Date of Birth <?= $required_field; ?></label>
+                                            <input type="date" class="form-control" id="" required >
+                                        </div>
+                                        <div class="col-2">
+                                            <label for="" class="form-label">Age <?= $required_field; ?></label>
+                                            <input type="number" class="form-control" id="" required>
+                                        </div>
+                                        <div class="col">
+                                            <label for="middlename" class="form-label">Civil Status <?= $required_field; ?></label>
+                                            <select class="form-control" required>
+                                                <option value="">Select</option> 
+                                                <?php foreach($civil_status as $row):?> 
+                                                    <option value=""><?= $row ?></option>  
+                                                <?php endforeach; ?>
+                                            </select>
+                                        </div>
+                                        <div class="col">
+                                            <label for="suffix" class="form-label">Sex <?= $required_field; ?></label>
+                                            <select class="form-control" required>
+                                                <option value="">Select</option> 
+                                                <option value="Male">Male</option> 
+                                                <option value="Female">Female</option>  
+                                            </select>
+                                        </div> 
+                                    </div> 
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <label for="" class="form-label">Contact #</label>
+                                            <input type="text" class="form-control" id="">
+                                        </div>
+                                        <div class="col-6">
+                                            <label for="" class="form-label">CTC # <?= $required_field; ?></label>
+                                            <input type="text" class="form-control" id="" required>
+                                        </div> 
+                                    </div> 
+                                    <div class="row" >
+                                        <div class="col-6">
+                                            <label for="" class="form-label">Email Address</label>
+                                            <input type="email" class="form-control" id="" parsley-type="email" >
+                                        </div>
+                                        <div class="col-6">
+                                            <label for="" class="form-label">Availment <?= $required_field; ?></label>
+                                            <input type="number" class="form-control" id="" required>
+                                        </div> 
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="row justify-content-center  ">   
+                                        <label for="formFileTvet" >
+                                            <img id="frameTvet" title="Select Image" class="rounded mx-auto d-block " alt="Profile Photo" src="<?=base_url()?>/img/select-image.png" style="  height: 240px !important; width: 250px !important"  /> 
+                                        </label> 
+                                        <input class="form-control" type="file" id="formFileTvet" style="display: none "> 
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="button" id="clearImageTvet" class="btn btn-primary mt-3 rounded-pill">Clear Photo</button>  
+                                    </div>
+                                </div>
+                            </div>
+                                
+                            <div class="row g-3" >
+                                <div class="col">
+                                    <label for="" class="form-label">School <?= $required_field; ?></label>
+                                    <select class="form-control" required>
+                                        <option value="">Select</option> 
+                                        <?php foreach($school as $row):?> 
+                                            <?php if($row['SchoolName'] != ""):?> 
+                                                <option value="<?= $row['ID']  ?>"><?= $row['SchoolName']  ?></option>  
+                                            <?php endif; ?>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <label for="" class="form-label">Course <?= $required_field; ?></label>
+                                    <select class="form-control" required>
+                                        <option value="">Select</option> 
+                                        <?php foreach($course as $row):?> 
+                                            <?php if($row['colCourse'] != ""):?> 
+                                                <option value="<?= $row['ID']  ?>"><?= $row['colCourse']  ?></option>  
+                                            <?php endif; ?>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div> 
+                            </div> 
+                            <div class="row g-3" >
+                                <div class="col">
+                                    <label for="" class="form-label">School Address </label> 
+                                    <input type="text" class="form-control" id="" required>
+                                </div>
+                            </div>
+                            <div class="row g-3" >
+                                <div class="col">
+                                    <label for="" class="form-label">Year Level <?= $required_field; ?></label>
+                                    <select class="form-control" required>
+                                        <option value="">Select</option> 
+                                        <?php foreach($year_level as $row):?>  
+                                            <option value="<?= $row   ?>"><?= $row   ?></option>   
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <label for="" class="form-label">Semester <?= $required_field; ?></label>
+                                    <select class="form-control" required>
+                                        <option value="">Select</option> 
+                                        <?php foreach($semester as $row):?>  
+                                            <option value="<?= $row   ?>"><?= $row   ?></option>   
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div>
+                                <div class="col">
+                                    <label for="" class="form-label">No. of Hours <?= $required_field; ?></label>
+                                    <input type="number" class="form-control" id="" required>
+                                </div>
+                                <div class="col">
+                                    <label for="" class="form-label">SY <?= $required_field; ?></label>
+                                    <select class="form-control" required>
+                                        <option value="">Select</option> 
+                                        <?php foreach(range(2017, date('Y')) as $year):?>  
+                                            <option value="SY: <?= $year . "-" . ($year + 1)?>">SY: <?= $year . "-" . ($year + 1)?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </div> 
+                            </div>
+                            <div class="row g-3" >
+                                <div class="col">
+                                    <label for="" class="form-label">Father's  Name</label>
+                                    <input type="text" class="form-control" id="">
+                                </div>
+                                <div class="col">
+                                    <label for="" class="form-label">Occupation</label>
+                                    <input type="text" class="form-control" id="">
+                                </div> 
+                            </div>
+                            <div class="row g-3" >
+                                <div class="col">
+                                    <label for="" class="form-label">Mother's  Name</label>
+                                    <input type="text" class="form-control" id="">
+                                </div>
+                                <div class="col">
+                                    <label for="" class="form-label">Occupation</label>
+                                    <input type="text" class="form-control" id="">
+                                </div> 
+                            </div>  
+                            <div class="row g-3 mt-2" > 
+                                    <button type="submit" class="btn btn-primary btn-block waves-effect waves-light rounded-pill">Save  Changes</button> 
+                            </div>    
+                        </form>
 
                     </div>
                 </div>
@@ -464,6 +643,27 @@
                 
                 document.getElementById('formFileShs').value = null;
                 frameCollege.src = "<?=base_url()?>/img/select-image.png";
+            });
+
+            
+            $(document).on('change', '#formFileTvet', function(e){  
+
+                var validExtensions = ["jpg","pdf","jpeg","gif","png", "jfif"]
+                var file = $(this).val().split('.').pop();
+                if (validExtensions.indexOf(file) == -1) { 
+                    Swal.fire({
+                        title:"Upload Error!",
+                        text: "Only formats are allowed : "+validExtensions.join(', '),
+                        icon:"error"
+                    }) 
+                }else{
+                    frameTvet.src = URL.createObjectURL(event.target.files[0]);
+                } 
+            }); 
+            $(document).on('click', '#clearImageTvet', function(e){ 
+
+                document.getElementById('formFileShs').value = null;
+                frameTvet.src = "<?=base_url()?>/img/select-image.png";
             });
              
             
