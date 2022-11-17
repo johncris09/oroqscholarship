1b9db138650cc5784ab24a652be48cd84be8d1c2
John Cris <johncrismanabo9@gmail.com>
2022-11-17 09:35:36 +0800

    custom config

---
+++ app/Config/Custom_config.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig; 
+
+class Custom_config extends BaseConfig
+{
+     
+    public $barangay = [
+        "Apil",
+        "Binuangan",
+        "Bolibol",
+        "Buenavista",
+        "Bunga",
+        "Buntawan",
+        "Burgos",
+        "Canubay",
+        "Clarin Settlement",
+        "Dolipos Bajo",
+        "Dolipos Alto",
+        "Dulapo",
+        "Dullan Norte",
+        "Dullan Sur",
+        "Lower Lamac",
+        "Layawan",
+        "Lower Langcangan",
+        "Lower Loboc",
+        "Lower Rizal",
+        "Malindang",
+        "Mialen",
+        "Mobod",
+        "Ciriaco Pastrano",
+        "Paypayan",
+        "Pines",
+        "Poblacion 1",
+        "Poblacion 2",
+        "Proper Langcangan",
+        "San Vicente Alto",
+        "San Vicente Bajo",
+        "Sebucal",
+        "Senote",
+        "Taboc Norte",
+        "Taboc Sur",
+        "Talairon",
+        "Talic",
+        "Toliyok",
+        "Tipan",
+        "Transville",
+        "Tuyabang Alto",
+        "Tuyabang Bajo",
+        "Tuyabang Proper",
+        "Upper Langcangan",
+        "Upper Lamac",
+        "Upper Loboc",
+        "Upper Rizal",
+        "Victoria",
+        "Villaflor" 
+    ];
+
+    public $civilStatus = [
+        "SINGLE",
+        "MARRIED",
+        "WIDOWED"
+    ];
+
+    public $gradeLevel = [
+        "Grade 11",
+        "Grade 12"
+    ];
+
+    
+    public $semester = [
+        "1st",
+        "2nd"
+    ];
+
+} 
