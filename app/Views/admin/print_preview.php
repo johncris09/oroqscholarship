5e7f42db56f848c7a6067a14b9e492c2ba930faf
John Cris <johncrismanabo9@gmail.com>
2023-01-17 10:22:07 +0800

    update meta data

--- app/Views/admin/print_preview.php
+++ app/Views/admin/print_preview.php
@@ -2,6 +2,14 @@
 <html>
 
 <head>
+  
+    <meta charset="utf-8" />
+    <title>Print Preview</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="<?= '/img/favicon.ico' ?>">
+
     <style>
         /* Styles go here */
       .page-header, .page-header-space {
